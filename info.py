b6bcbf960a79d64c82442afa68814569461b1f09
bhoiishwar27-create <bhoiishwar27@gmail.com>
2026-02-05 19:57:30 +0530

    Update info.py

--- info.py
+++ info.py
@@ -19,7 +19,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', '8396674527:AAEawX1s5GFB4jFiFG4E7sV9s6Hx2UT
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '8286715829').split()]
-USERNAME = environ.get('USERNAME', "https://t.me/Innocent_babe_dead") # ADMIN USERNAME
+USERNAME = environ.get('USERNAME', "https://t.me/@IBmovie_x") # ADMIN USERNAME
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1003779146584'))
 MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/+K2kgCBgaat80YWQ9')
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1003744901357').split()]
